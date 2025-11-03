020a3282228feb3ab2ef1153c052b881afa1d98d
unknown <vgviji2003@gmail.com>
2025-11-03 22:17:56 +0530

    Save local changes before pulling

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/zeal-taste/', // 👈 your repo name here
+  base: 'zeal-taste',
 })
